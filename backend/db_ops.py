fea2b4637b23ff48647287d59a651fd7c5333454
HAL9000COM <f1226942353@icloud.com>
2024-03-01 14:24:36 +0800

    Add database search.

--- backend/db_ops.py
+++ backend/db_ops.py
@@ -1,110 +1,111 @@
 # %%
 import sqlite3
-import sqlite_spellfix
+from pathlib import Path
 
-
-# Connect to the database (creates a new database if it doesn't exist)
-conn = sqlite3.connect("example.db")
-conn.enable_load_extension(True)
-conn.load_extension(sqlite_spellfix.extension_path())  # type:ignore
-
-# Create a cursor object to execute SQL queries
-cursor = conn.cursor()
-
-# Create a table if it doesn't exist
-cursor.execute(
-    """
-    CREATE TABLE IF NOT EXISTS notes (
-        id INTEGER PRIMARY KEY,
-        language TEXT,
-        text TEXT
-    )
+TABLE_SQL = """
+CREATE TABLE IF NOT EXISTS pictures (
+    id INTEGER PRIMARY KEY,
+    hash TEXT,
+    path TEXT,
+    classification TEXT,
+    classification_confidence REAL,
+    object TEXT,
+    object_confidence REAL,
+    OCR TEXT,
+    ocr_confidence REAL,
+    created_at INTEGER DEFAULT (strftime('%s', 'now'))
+);
 """
-)
-
-# Insert data into the table
-cursor.executemany(
-    "INSERT INTO notes (language, text) VALUES (?, ?)",
-    [
-        ("English", "Wikipedia the free encyclopedia"),
-        ("Spanish", "Wikipedia la enciclopedia libre"),
-        ("French", "Wikipédia l'encyclopédie libre"),
-        ("German", "Wikipedia die freie Enzyklopädie"),
-        ("Chinese (Simplified)", "维基百科，自由的百科全书"),
-        ("Chinese (Traditional)", "維基百科，自由的百科全書"),
-        ("Japanese", "ウィキペディア、自由な百科事典"),
-        ("Arabic", "ويكيبيديا الموسوعة الحرة"),
-        ("Russian", "Википедия свободная энциклопедия"),
-        ("Hindi", "विकिपीडिया, एक मुक्त विज्ञानकोश"),
-    ],
-)
-
-# Commit the changes to the database
-conn.commit()
-
-
-# %%
-# Retrieve data from the table
-def init_search(cursor):
-    # Create a virtual table for full-text search
-    cursor.execute(
-        """
-        CREATE VIRTUAL TABLE IF NOT EXISTS search_index USING fts5(content);
-    """
-    )
-
-    # Populate the search index with data from the users table
-    cursor.execute(
-        """
-        INSERT INTO search_index(content) SELECT text FROM notes;
-    """
-    )
-
-    # Enable the spellfix1 extension
-    cursor.execute(
-        """
-        CREATE VIRTUAL TABLE IF NOT EXISTS spellfix USING spellfix1;
-    """
-    )
-    # add vocabulary to spellfix from search_index
-    cursor.execute(
-        """
-        INSERT INTO spellfix(word) SELECT content FROM search_index;
-    """
-    )
+SEARCH_TABLE_SQL = """
+CREATE VIRTUAL TABLE IF NOT EXISTS pictures_fts USING fts5(
+    id,
+    classification,
+    object,
+    OCR,
+    content = pictures,
+    content_rowid = id,
+    tokenize = "simple"
+);
+"""
+TRIGGER_SQL = """
+CREATE TRIGGER IF NOT EXISTS pictures_ai AFTER INSERT ON pictures BEGIN
+    INSERT INTO pictures_fts(rowid, classification, object, OCR) VALUES (new.id, new.classification, new.object, new.OCR);
+END;
+CREATE TRIGGER IF NOT EXISTS pictures_ad AFTER DELETE ON pictures BEGIN
+    INSERT INTO pictures_fts(pictures_fts, rowid, classification, object, OCR) VALUES('delete', old.id, old.classification, old.object, old.OCR);
+END;
+CREATE TRIGGER IF NOT EXISTS pictures_au AFTER UPDATE ON pictures BEGIN
+    INSERT INTO pictures_fts(pictures_fts, rowid, classification, object, OCR) VALUES('delete', old.id, old.classification, old.object, old.OCR);
+    INSERT INTO pictures_fts(rowid, classification, object, OCR) VALUES (new.id, new.classification, new.object, new.OCR);
+END;
+"""
+SEARCH_SIMPLE_SQL = """
+SELECT * FROM pictures WHERE id IN (SELECT id FROM pictures_fts WHERE pictures_fts MATCH simple_query(?) ORDER BY rank);
+"""
+INIT_JIEBA_SQL = """
+SELECT jieba_dict(?);
+"""
+SEARCH_JIEBA_SQL = """
+SELECT * FROM pictures WHERE id IN (SELECT id FROM pictures_fts WHERE pictures_fts MATCH jieba_query(?) ORDER BY rank);
+"""
+INSERT_SQL = """
+INSERT INTO pictures (hash, path, classification, classification_confidence, object, object_confidence, OCR, ocr_confidence)
+VALUES (?, ?, ?, ?, ?, ?, ?, ?);
+"""
 
 
-init_search(cursor)
-# Perform a spell correction query
-# %%
-cursor.execute(
-    """
-    SELECT word FROM spellfix WHERE word MATCH ?;
-""",
-    ("百科",),
-)
+class DB:
+    def __init__(self, jieba=False):
 
-# Retrieve the corrected word
-corrected_word = cursor.fetchone()[0]
+        extention_path = (
+            Path(__file__).resolve().parent / "libsimple-windows-x64" / "simple"
+        )
+        dict_path = Path(__file__).resolve().parent / "libsimple-windows-x64" / "dict"
 
-# Perform a full-text search query
-cursor.execute(
-    """
-    SELECT * FROM notes WHERE id IN (
-        SELECT rowid FROM search_index WHERE content MATCH ?
-    );
-""",
-    (corrected_word,),
-)
+        self.conn = sqlite3.connect(":memory:")
+        self.conn.enable_load_extension(True)
+        self.conn.load_extension(extention_path.as_posix())
+        self.conn.execute(TABLE_SQL)
+        self.conn.execute(SEARCH_TABLE_SQL)
+        self.conn.executescript(TRIGGER_SQL)
+        self.jieba = jieba
+        if jieba:
+            self.init_jieba(dict_path.as_posix())
 
-# Retrieve the search results
-search_results = cursor.fetchall()
+    def search(self, query):
+        if self.jieba:
+            return self.conn.execute(SEARCH_JIEBA_SQL, (query,)).fetchall()
+        else:
+            return self.conn.execute(SEARCH_SIMPLE_SQL, (query,)).fetchall()
 
-# Print the search results
-for result in search_results:
-    print(result)
+    def init_jieba(self, dict_path):
+        self.conn.execute(INIT_JIEBA_SQL, (dict_path,))
 
-# Close the cursor and the connection
-cursor.close()
-conn.close()
-# %%
+    def insert(
+        self,
+        hash,
+        path,
+        classification,
+        classification_confidence,
+        object,
+        object_confidence,
+        OCR,
+        ocr_confidence,
+    ):
+        self.conn.execute(
+            INSERT_SQL,
+            (
+                hash,
+                path,
+                classification,
+                classification_confidence,
+                object,
+                object_confidence,
+                OCR,
+                ocr_confidence,
+            ),
+        )
+        self.conn.commit()
+
+    def close(self):
+        self.conn.close()
