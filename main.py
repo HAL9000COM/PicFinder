7065d4734414a05054806a3d4fac3b1d052a2646
HAL9000COM <f1226942353@icloud.com>
2024-04-17 20:07:34 +0800

    cx_Freeze script and gitignore.

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 
-import logging
+
 import os
 import sys
+
+if sys.stdout is None:
+    # sys.stdout = open(os.devnull, "w")
+    sys.stdout = open("stdout.log", "w")
+if sys.stderr is None:
+    # sys.stderr = open(os.devnull, "w")
+    sys.stderr = open("stderr.log", "w")
+import logging
 from multiprocessing import freeze_support
 
 from PySide6.QtWidgets import QApplication
