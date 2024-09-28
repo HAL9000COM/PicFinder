069b444e886627bf703638957a8bacb9062d8c95
HAL9000COM <f1226942353@icloud.com>
2024-09-28 22:19:49 +0800

    Script for cicd.

--- src/dev/download_libsimple.py
+++ src/dev/download_libsimple.py
@@ -1,7 +1,7 @@
-# %%
 # download libsimple release from github
 import os
 import shutil
+import sys
 import zipfile
 from pathlib import Path
 
@@ -10,12 +10,30 @@ import requests
 
 def download_libsimple():
     # Dictionary containing the target libraries and their download URLs
-    download_target = {
+    url_dict = {
+        "libsimple-aarch64-linux-gnu-gcc-9": "https://github.com/wangfenjin/simple/releases/download/v0.4.0/libsimple-aarch64-linux-gnu-gcc-9.zip",
         "libsimple-linux-ubuntu-latest": "https://github.com/wangfenjin/simple/releases/download/v0.4.0/libsimple-linux-ubuntu-latest.zip",
         "libsimple-windows-x64": "https://github.com/wangfenjin/simple/releases/download/v0.4.0/libsimple-windows-x64.zip",
     }
-    for lib_dir_name, url in download_target.items():
+    # get platform and architecture
+    if sys.platform.startswith("win"):
+        # check if the system is 32 or 64 bit
+        lib_to_download = "libsimple-windows-x64"
+    elif sys.platform.startswith("linux"):
+        # check if the system is arm or x86
+        if "aarch64" in os.uname().machine:
+            lib_to_download = "libsimple-aarch64-linux-gnu-gcc-9"
+        else:
+            lib_to_download = "libsimple-linux-ubuntu-latest"
+    else:
+        # exit if the platform is not supported
+        print(f"Unsupported OS: {sys.platform}")
+        # Download and extract the libraries
+    if lib_to_download in url_dict:
+        url = url_dict[lib_to_download]
+        lib_dir_name = lib_to_download
         try:
+            print(f"Downloading {url}...")
             r = requests.get(url)
             r.raise_for_status()  # Check if the request was successful
             with open("libsimple.zip", "wb") as f:
@@ -36,6 +54,5 @@ def download_libsimple():
             print(f"An error occurred: {e}")
 
 
-# %%
-download_libsimple()
-# %%
+if __name__ == "__main__":
+    download_libsimple()
